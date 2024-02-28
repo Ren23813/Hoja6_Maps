441045af4ce239c6fd3f59f8514698c3aaaeb919
belennmm <belenmmonterroso@gmail.com>
2024-02-28 08:38:04 -0600

    Creación de Clases

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Método principal, ejecuta el programa.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Funcionalidades Funcionalidades = new Funcionalidades();
+       
+    }
+
+}
