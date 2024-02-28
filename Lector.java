a4c8107a435e4067e5d5d209392e14ee1c4273c7
belennmm <belenmmonterroso@gmail.com>
2024-02-28 09:14:11 -0600

    menu

--- Lector.java
+++ Lector.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Lector {
+
+    String archivo = "cards_desc.txt";
+
+    public String[] leerDatosArchivo(String archivo) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String line;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
 
 }
--- Main.java
+++ Main.java
@@ -9,8 +9,58 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
         Funcionalidades Funcionalidades = new Funcionalidades();
-       
+
+        String MapType = null;
+
+
+        while(true){
+            System.out.println("\n------------------------------------");
+            System.out.println("\nIMPLEMENTACIÓN MAP");
+            System.out.println("1. HashMap");
+            System.out.println("2. TreeMap");
+            System.out.println("3. LinkedHashMAp");
+
+            System.out.print("\nIngrese la opción que desee: ");
+          
+
+            String op = scan.nextLine();
+
+            switch(op){
+
+                case "1":
+                    //Implementación de HashMap
+                    System.out.println("\nHashMap");
+                    MapType = "HashMap";
+                    System.out.println("Tipo de stack seleccionado: " + MapType);
+                    break;
+                
+                case "2":
+                    //Implementación de TreeMap
+                    System.out.println("\nTreeMap");
+                    MapType = "TreeMap";
+                    System.out.println("Tipo de stack seleccionado: " + MapType);
+                    break;
+
+                case "3":
+                    //Implementación de LinkedHashMap
+                    System.out.println("\nLinkedHashMap");
+                    MapType = "LinkedHashMap";
+                    System.out.println("Tipo de stack seleccionado: " + MapType);
+                    break;
+
+                default:
+                    System.out.println("\nLa opción ingresada no es válida.");
+                    break;
+                
+                    
+            }
+            
+        }
+
+
     }
 
 }
